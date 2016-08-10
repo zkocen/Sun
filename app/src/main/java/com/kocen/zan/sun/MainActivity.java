481a918751747c0d473ea2bb7da6cd9d6d118dcd
zkocen <zkocen@gmail.com>
2016-08-10 19:34:03 +0100

    working with settings added to menu

--- app/src/main/java/com/kocen/zan/sun/MainActivity.java
+++ app/src/main/java/com/kocen/zan/sun/MainActivity.java
@@ -3,13 +3,12 @@ package com.kocen.zan.sun;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ForecastFragment())
