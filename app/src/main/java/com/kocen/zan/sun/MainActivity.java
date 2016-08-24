26272ec81179b91902c2f8f19951c9ba5b2af52e
zkocen <zkocen@gmail.com>
2016-08-24 19:46:04 +0100

    db added with 4 tests

--- app/src/main/java/com/kocen/zan/sun/MainActivity.java
+++ app/src/main/java/com/kocen/zan/sun/MainActivity.java
@@ -3,14 +3,17 @@ package com.kocen.zan.sun;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(LOG_TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         if(savedInstanceState == null){
@@ -20,4 +23,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onPause() {
+        Log.v(LOG_TAG, "onPause() ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v(LOG_TAG, "onStop() ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.v(LOG_TAG, "onResume() ");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.v(LOG_TAG, "onStart() ");
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.v(LOG_TAG, "onDestroy() ");
+        super.onDestroy();
+    }
 }
